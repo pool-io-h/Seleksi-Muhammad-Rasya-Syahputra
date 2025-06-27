69c995eddd1f50ebd7f0fc208d9c49bb820a52f7
pool-io-h <mrasyahputra2006@gmail.com>
2025-06-27 09:26:08 +0700

    Added menu WIP

---
+++ scripts/Menu.js
@@ -0,0 +1,20 @@
+const buttonHTML = '';
+
+const menu = [
+  {
+    plan : 'Diet Plan',
+    price : 30, // thousand
+    photo1 : 'Images/food-card1.jpg',
+    photo2 : 'Images/food-protein2.avif',
+    photo3 : 'Images/food-protein3.jpg',
+    photo4 : 'Images/food-protein4.jpg',
+    desc : 'Want to lose weight or just live healthier without giving up the taste of Indonesian food? This diet plan is designed especially for you! With local ingredients that are easy to find and filling menu options, you can say goodbye to restrictive eating and hello to a balanced, delicious, and sustainable way of reaching your health goals!',
+  }
+]
+
+document.querySelectorAll('.details')
+  .forEach((button) => {
+    button.addEventListener('click', (index) => {
+
+    })
+  })  
